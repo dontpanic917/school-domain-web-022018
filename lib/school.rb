fa987c437b0aea0ce0bc1c7778e42d3a7f7436a3
Grayson Cowing <grayson.cowing@gmail.com>
2018-02-22 15:10:28 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,11 @@
 # code here!
+class School
+  attr_accessor :roster
+
+  def initialize(schoolname)
+    @schoolname=name
+    roster=[]
+  end
+
+  def add_student(student, grade)
+    roster << {student=>grade}
